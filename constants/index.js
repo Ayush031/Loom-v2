3c4eae575c064f564e29ae4434e2e5e079274796
Ayush <ayushkansal303@gmail.com>
2024-09-18 22:35:06 +0530

    initial project commmit

---
+++ constants/index.js
@@ -0,0 +1,89 @@
+export const exploreWorlds = [
+  {
+    id: "world-1",
+    imgUrl: "/planet-01.png",
+    title: "WorkSphere"
+  },
+  {
+    id: "world-2",
+    imgUrl: "/planet-02.png",
+    title: "NurtureNest"
+  },
+  {
+    id: "world-3",
+    imgUrl: "/planet-03.png",
+    title: "Fortune's Haven"
+  },
+  {
+    id: "world-4",
+    imgUrl: "/planet-04.png",
+    title: "Subterra City"
+  },
+  {
+    id: "world-5",
+    imgUrl: "/planet-05.png",
+    title: "Compassion Cove"
+  }
+];
+
+
+export const startingFeatures = [
+  'Install and register AO Connect Wallet to begin.',
+  'Find a world that suits you and you want to enter',
+  'Enter the world and enjoy Multiplayer Microgames',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: '',
+    subtitle:
+      'we have the latest update with New Worlds for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+      'In the latest update, your get access to exclusive Roleplay Simulations'
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
+    subtitle:
+      'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 tips to easily master the madness of the Metaverse',
+    subtitle:
+      'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'With one platform you can explore the whole world virtually',
+    subtitle:
+      'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
